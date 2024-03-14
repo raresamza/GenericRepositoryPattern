b9e0490d65d9f5828f2190787e239b623f361ae4
rares.amza <rares.amza@ROMOB41159>
2024-03-14 10:53:29 +0200

    Repository complete

---
+++ RepoApp/RepoApp/GenericRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoApp
+{
+    public class GenericRepository<T> : IRepository<T>
+    {
+
+        public List<T> Entities { get; set; }
+        public GenericRepository()
+        {
+            Entities = new List<T>();
+        }
+
+        public void Add(T entity)
+        {
+            Entities.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            Entities.Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Entities;
+        }
+
+        public T GetById(int id)
+        {
+            foreach (T entity in Entities)
+            {
+                var idProperty=entity.GetType().GetProperty("ID");
+                if(idProperty != null && (int) idProperty.GetValue(entity)==id)
+                {
+                    return entity;
+                }
+            }
+
+            return default;
+        }
+
+        public void Update(T newEntity)
+        {
+            int index = -1;
+            for(int i=0;i<Entities.Count; i++)
+            {
+                var entity = Entities[i];
+                var id=entity.GetType().GetProperty("ID");
+                if (id != null && (int)id.GetValue(entity) == (int)id.GetValue(newEntity))
+                {
+                    index = i;
+                    break;
+                }
+                if (index != -1) {
+                    Entities[i] = newEntity;
+                }
+            }
+        }
+    }
+}
---
+++ RepoApp/RepoApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using RepoApp;
+
+var productRepo=new GenericRepository<Product>();
+
+productRepo.Add(new Product { ID = 1, Name = "Banana", Price = 2 });
+productRepo.Add(new Product { ID = 2, Name = "Pineapple", Price = 5 });
+
+Console.WriteLine("The products are:");
+foreach(var product in productRepo.GetAll())
+{
+    Console.WriteLine($"ID: {product.ID}, Name: {product.Name}, Price: {product.Price}");
+}
+
+var updatedProduct=productRepo.GetById(1);
+if (productRepo != null) {
+    updatedProduct.Name = "Celery";
+    productRepo.Update(updatedProduct);
+} else
+{
+    Console.WriteLine("Product not found");
+}
+Console.WriteLine("Updated Product is:");
+Console.WriteLine($"ID: {productRepo.GetById(1).ID}, Name: {productRepo.GetById(1).Name}, Price {productRepo.GetById(1).Price}");
+var deletedProduct = productRepo.GetById(2);
+if (deletedProduct != null) { 
+    productRepo.Delete(deletedProduct);
+}
+Console.WriteLine("After deleting the products are:");
+foreach (var product in productRepo.GetAll())
+{
+    Console.WriteLine($"ID: {product.ID}, Name: {product.Name}, Price: {product.Price}");
+}
